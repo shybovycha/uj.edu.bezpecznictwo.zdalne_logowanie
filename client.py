2f8844cadc04463de497be21b5eac16f34a8b40c
Artem Shoobovych <shybovycha@gmail.com>
2014-10-29 16:50:08 +0100

    Added changing password and logging out features

--- client.py
+++ client.py
@@ -22,11 +22,37 @@ class MyClient:
         if response == 'ok':
             self.is_logged_in = True
 
-        return self.is_logged_in
+        return response == 'ok'
+
+    def logout(self):
+        command = pickle.dumps({ "command": "logout" })
+
+        self.socket.send(command)
+        response = self.socket.recv(self.buf_size)
+
+        if response == 'ok':
+            self.is_logged_in = False
+
+        return response == 'ok'
+
+    def change_password(self, new_password):
+        command = pickle.dumps({ "command": "chpwd", "new_password": new_password })
+
+        self.socket.send(command)
+        response = self.socket.recv(self.buf_size)
+
+        return response == 'ok'
 
     def run(self):
-        print "user1 : abc123 = %s" % self.login("user1", "abc123")
-        print "user1 : abc124 = %s" % self.login("user1", "abc124")
+        # print "user1 : abc123 = %s" % self.login("user1", "abc123")
+        # print "user1 : abc124 = %s" % self.login("user1", "abc124")
+
+        print "login: %s" % self.login("user1", "abc123")
+        print "chpwd: %s" % self.change_password("abc125")
+        print "logout: %s" % self.logout()
+        print "login again: %s" % self.login("user1", "abc125")
+        print "logout: %s" % self.logout()
+        print "login wrongly: %s" % self.login("user1", "abc123")
 
         # on exit
         self.socket.close()
--- server.py
+++ server.py
@@ -3,19 +3,20 @@ import md5
 import threading
 import pickle
 
-def encode_password(password):
-    return md5.new(password).digest()
-
 class User:
     users = {
-            "user1": encode_password("abc123"),
-            "user2": encode_password("password_123"),
-            "user3": encode_password("password_abc")
+            "user1": "abc123",
+            "user2": "password_123",
+            "user3": "password_abc"
         }
 
     @staticmethod
     def check_login(username, password):
-        return (username in User.users and User.users[username] == encode_password(password))
+        return (username in User.users and User.users[username] == password)
+
+    @staticmethod
+    def change_password(username, new_password):
+        User.users[username] = new_password
 
 class MyServer:
     def __init__(self, host = 'localhost', port = 5000):
@@ -23,13 +24,6 @@ class MyServer:
         self.port = port
         self.backlog = 5
         self.buf_size = 1024
-
-        self.users = {
-            "user1": encode_password("abc123"),
-            "user2": encode_password("password_123"),
-            "user3": encode_password("password_abc")
-        }
-
         self.clients = []
 
         print "Starting the server..."
@@ -67,34 +61,60 @@ class MyClient(threading.Thread):
         self.address = address
         self.buf_size = 1024
 
+        self.is_logged_in = False
+
         print "New client is connected!"
 
     def run(self):
-        while True:
-            data_str = self.socket.recv(self.buf_size)
+        try:
+            while True:
+                data_str = self.socket.recv(self.buf_size)
 
-            # empty data received
-            if not data_str:
-                break
+                # empty data received
+                if not data_str:
+                    break
 
-            data = pickle.loads(data_str)
+                data = pickle.loads(data_str)
 
-            if not ("command" in data):
-                raise Exception("Invalid packet")
+                if not ("command" in data):
+                    raise Exception("Invalid packet")
 
-            if (data["command"] == "login"):
-                if not ("username" in data and "password" in data):
-                    raise Exception("Invalid login arguments")
+                if (data["command"] == "login"):
+                    if not ("username" in data and "password" in data):
+                        #raise Exception("Invalid login arguments")
+                        self.socket.send("invalid login arguments")
+                        continue
 
-                is_login_correct = User.check_login(data["username"], data["password"])
+                    is_login_correct = User.check_login(data["username"], data["password"])
 
-                if (is_login_correct):
+                    if (is_login_correct):
+                        self.username = data["username"]
+                        self.is_logged_in = True
+
+                        self.socket.send("ok")
+                    else:
+                        self.socket.send("error")
+                        self.is_logged_in = False
+                elif (data["command"] == "logout"):
+                    self.is_logged_in = False
                     self.socket.send("ok")
-                else:
-                    self.socket.send("error")
+                elif (data["command"] == "chpwd"):
+                    if not self.is_logged_in:
+                        # raise Exception("You are not logged in")
+                        self.socket.send("you are not logged in")
+                        continue
 
-        # on exit
-        self.socket.close()
+                    if not ("new_password" in data):
+                        #raise Exception("Invalid chpass arguments")
+                        self.socket.send("invalid chpass arguments")
+                        continue
+
+                    User.change_password(self.username, data["new_password"])
+
+                    self.socket.send("ok")
+        finally:
+            # on exit
+            self.socket.close()
 
 if __name__ == "__main__":
     server = MyServer()
